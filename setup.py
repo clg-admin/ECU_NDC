a22f9d5399af9606f5eb868eb1ac6ac4d607d2c8
Susana-Solorzano-Jimenez <ssolorzano@clg-cr.com>
2025-01-17 10:40:57 -0600

    Cambio de estructura

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     try:
         setup(
             use_scm_version={"version_scheme": "no-guess-dev"},
-            packages=["IPPU", "Waste"],  # Lista explícita de paquetes
+            packages=["IPPU", "Waste", "Energy"],  # Lista explícita de paquetes
             include_package_data=True,
         )
     except:  # noqa
